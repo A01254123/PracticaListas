8e9a6b3a4330fd57b4afcbd0085ae27eabf36b01
A01254123 <A01254123@itesm.mx>
2021-09-28 16:31:27 +0000

    Lista

---
+++ assignments/Listas/src/exercise.py
@@ -0,0 +1,8 @@
+def main():
+    #escribe tu código abajo de esta línea
+    lista=["Matemáticas","Física","Química","Historia","Lengua"]
+    print(lista)
+    
+
+if __name__=='__main__':
+    main()
